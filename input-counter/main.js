974b54f4a0b9a7efccbe7ee43d47be15aa5791b8
Sakmonysothea Youk <youksakmonysothea@gmail.com>
2019-08-29 16:59:43 +0700

    change sum to output

--- input-counter/main.js
+++ input-counter/main.js
@@ -4,24 +4,24 @@ const resetButton = document.getElementById('reset')
 const numberElement = document.getElementById('number')
 const getInput = document.getElementById('numberInput')
 
-var sum = 0
+var output = 0
 
 function add(){
-    sum += parseInt(getInput.value)
-    numberElement.textContent = sum
+    output += parseInt(getInput.value)
+    numberElement.textContent = output
     document.getElementById('numberInput').value = ""
 }
 function subtract(){
-    sum -= parseInt(getInput.value)
-    numberElement.textContent = sum
+    output -= parseInt(getInput.value)
+    numberElement.textContent = output
     document.getElementById('numberInput').value = ""
 }
 function reset(){
-    sum = 0
-    numberElement.textContent = sum
+    output = 0
+    numberElement.textContent = output
     document.getElementById('numberInput').value = ""
 }
 
-plusButton.addEventListener('click', () => add())
-minusButton.addEventListener('click', () => subtract())
-resetButton.addEventListener('click', () => reset())
+plusButton.addEventListener('click', add)
+minusButton.addEventListener('click', subtract)
+resetButton.addEventListener('click', reset)
