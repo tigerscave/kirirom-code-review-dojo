dd5a293bf0a201c86adedb5c113c9127b38ef489
MYSELF <sunchenny17@kit.edu.kh>
2019-08-29 16:55:42 +0700

    modify the code

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,24 +1,23 @@
 var numberElement = document.getElementById("number")
 var inputElement = document.getElementById("numberInput")
-var inputValue = 0
 const plusButton = document.getElementById("plus")
 const minusButton = document.getElementById("minus")
 const resetButton = document.getElementById("reset")
 
 function plusClick(number){
-  numberElement.textContent = parseInt(numberElement.textContent) + parseInt(number)
+  numberElement.textContent = parseInt(numberElement.textContent) + parseInt(inputElement.value)
   inputElement.value = null
 }
 function minusClick(number){
-  numberElement.textContent = parseInt(numberElement.textContent) - parseInt(number)
+  numberElement.textContent = parseInt(numberElement.textContent) - parseInt(inputElement.value)
   inputElement.value = null
 }
 function resetClick(){
   numberElement.textContent = 0 
-  inputValue = null
+  inputElement.value = null
 }
 
-plusButton.addEventListener ('click', () => plusClick(inputValue = inputElement.value))
-minusButton.addEventListener ('click', () => minusClick(inputValue = inputElement.value))
+plusButton.addEventListener ('click', plusClick)
+minusButton.addEventListener ('click', minusClick)
 resetButton.addEventListener ('click', resetClick)
 
