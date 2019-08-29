84b5bf051342e8da606b0e51d5b93907897de5a6
Thepanom Ratanawan <thepanomratanawan17@kit.edu.kh>
2019-08-29 16:45:51 +0700

    input counter commit

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,30 @@
 // write conde in here
+const numberElement = document.getElementById('number')
+const numberInputValue = document.getElementById('numberInput')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+let number = 0
+
+function calculate(operator){
+    let input = parseInt(numberInputValue.value)
+    if(isNaN(input)){
+        input=0
+    }
+    if(operator == '+') {
+        number = number + input;
+    }
+    else if(operator == '-') {
+        number = number - input;
+    }
+    else{
+        number = 0;
+    }
+    numberElement.textContent = number
+    numberInputValue.value=''
+}
+
+plusButton.addEventListener('click', () => calculate('+'))
+minusButton.addEventListener('click', () => calculate('-'))
+resetButton.addEventListener('click', () => calculate('reset'))
