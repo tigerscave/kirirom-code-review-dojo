44b45339b4f6d66ca82a639a8dcf874a080dee0f
Dane <peldane17@kit.edu.kh>
2019-08-29 16:52:23 +0700

    Last commit

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,37 @@
 // write conde in here
+const outText = document.getElementById('number')
+const inputText = document.getElementById('numberInput')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+let num = 0
+let x = 0
+
+function plusNum() {
+    if(inputText.value != "") {
+        num = num + parseInt(inputText.value)
+    }
+    outText.textContent = num
+    inputText.focus()
+}
+
+function minusNum() {
+    if(inputText.value != "") {
+        num = num - parseInt(inputText.value)
+    }
+    outText.textContent = num
+    inputText.focus()
+}
+
+function reset() {
+    inputText.value = ""
+    num = 0
+    outText.textContent = num
+    inputText.focus()
+}
+
+
+resetButton.addEventListener('click', reset)
+plusButton.addEventListener('click', plusNum)
+minusButton.addEventListener('click', minusNum)
+
