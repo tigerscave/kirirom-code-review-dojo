efc4a706b7b9aca05d4b21a7447ea5ea8b53635e
songsokhavudthi <sokhavudthisong@gmail.com>
2019-08-29 16:23:35 +0700

    input counter

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,23 @@
-// write conde in here
+// write code in here
+
+let output = 0
+
+const numberElement = document.getElementById('number')
+const numberInput = document.getElementById('numberInput')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+const calculate = operation => () => {
+  operation == '+'
+    ? (output += parseInt(numberInput.value))
+    : operation == '-'
+    ? (output -= parseInt(numberInput.value))
+    : (output = 0)
+  numberElement.textContent = output;
+  numberInput.value = ''
+}
+
+plusButton.addEventListener('click', calculate('+'))
+minusButton.addEventListener('click', calculate('-'))
+resetButton.addEventListener('click', calculate(false))
