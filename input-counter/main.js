c7c163369039ac20c7d331b3ef373acc970764fc
Dara0505 <kruychandara17@kit.edu.kh>
2019-08-29 16:44:58 +0700

    fix indentation

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,27 +1,25 @@
-let num = 0
+let num = 0;
 
-const plusButton = document.getElementById('plus')
-const minusButton = document.getElementById('minus')
-const resetButton = document.getElementById('reset')
-const input = document.getElementById('numberInput')
-const number = document.getElementById('number')
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const resetButton = document.getElementById("reset");
+const input = document.getElementById("numberInput");
+const number = document.getElementById("number");
 
-const handleButtonClick = (symbol) => () => {
-  const val = parseInt(input.value)
-  if(Number.isNaN(val) && symbol !== '') return
-  if(symbol === '+') {
-	number.textContent = (num += val).toString()
+const handleButtonClick = symbol => () => {
+  const val = parseInt(input.value);
+  if (Number.isNaN(val) && symbol !== "") return;
+  if (symbol === "+") {
+    number.textContent = (num += val).toString();
+  } else if (symbol === "-") {
+    number.textContent = (num -= val).toString();
+  } else {
+    number.textContent = "0";
+    num = 0;
   }
-  else if(symbol === '-') {
-	number.textContent = (num -= val).toString()
-  }
-  else{
-	number.textContent = '0'
-	num = 0
-  }
-  input.value = null
-}
+  input.value = null;
+};
 
-plusButton.addEventListener('click', handleButtonClick('+'))
-minusButton.addEventListener('click', handleButtonClick('-'))
-resetButton.addEventListener('click', handleButtonClick(''))
+plusButton.addEventListener("click", handleButtonClick("+"));
+minusButton.addEventListener("click", handleButtonClick("-"));
+resetButton.addEventListener("click", handleButtonClick(""));
