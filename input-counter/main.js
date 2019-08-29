a0ed2775a590ee7754fb50bb18398a419010b84d
MYSELF <sunchenny17@kit.edu.kh>
2019-08-29 16:56:22 +0700

    remove argument

--- input-counter/main.js
+++ input-counter/main.js
@@ -4,11 +4,11 @@ const plusButton = document.getElementById("plus")
 const minusButton = document.getElementById("minus")
 const resetButton = document.getElementById("reset")
 
-function plusClick(number){
+function plusClick(){
   numberElement.textContent = parseInt(numberElement.textContent) + parseInt(inputElement.value)
   inputElement.value = null
 }
-function minusClick(number){
+function minusClick(){
   numberElement.textContent = parseInt(numberElement.textContent) - parseInt(inputElement.value)
   inputElement.value = null
 }
