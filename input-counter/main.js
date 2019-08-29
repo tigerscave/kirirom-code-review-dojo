3c6222fb6b6b9e42deb7e3f461990c1c707787b4
moonlight <miratorimoonlight@gmail.com>
2019-08-29 16:27:20 +0700

    input_counter4

--- input-counter/main.js
+++ input-counter/main.js
@@ -27,8 +27,8 @@ function minusButtonClick()
 
 function resetButtonClick()
 {
-    const number = document.getElementById('number')
-    const number_input = document.getElementById('numberInput')
+    var number = document.getElementById('number')
+    var number_input = document.getElementById('numberInput')
     number.textContent = 0
     number_input.value=''
 }
