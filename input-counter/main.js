aaa8f8a48efbeee25c9e8f39265d4a921bf99b79
MYSELF <sunchenny17@kit.edu.kh>
2019-08-29 16:46:58 +0700

    added functionalities to buttons

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,24 @@
-// write conde in here
+var numberElement = document.getElementById("number")
+var inputElement = document.getElementById("numberInput")
+var inputValue = 0
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+
+function plusClick(number){
+  numberElement.textContent = parseInt(numberElement.textContent) + parseInt(number)
+  inputElement.value = null
+}
+function minusClick(number){
+  numberElement.textContent = parseInt(numberElement.textContent) - parseInt(number)
+  inputElement.value = null
+}
+function resetClick(){
+  numberElement.textContent = 0 
+  inputValue = null
+}
+
+plusButton.addEventListener ('click', () => plusClick(inputValue = inputElement.value))
+minusButton.addEventListener ('click', () => minusClick(inputValue = inputElement.value))
+resetButton.addEventListener ('click', resetClick)
+
