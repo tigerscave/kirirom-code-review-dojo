7229465170d1e9d8238d116052008343049f5818
Sakmonysothea Youk <youksakmonysothea@gmail.com>
2019-08-29 16:52:54 +0700

    input counter 1st commit

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,26 @@
-// write conde in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const numberElement = document.getElementById('number')
+const getInput = document.getElementById('numberInput')
+
+var sum = 0
+
+function add(){
+    sum += parseInt(getInput.value)
+    numberElement.textContent = sum
+    document.getElementById('numberInput').value = ""
+}
+function subtract(){
+    sum -= parseInt(getInput.value)
+    numberElement.textContent = sum
+    document.getElementById('numberInput').value = ""
+}
+function reset(){
+    numberElement.textContent = 0
+    document.getElementById('numberInput').value = ""
+}
+
+plusButton.addEventListener('click', () => add())
+minusButton.addEventListener('click', () => subtract())
+resetButton.addEventListener('click', () => reset())
