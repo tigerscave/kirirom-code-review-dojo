b67773bab8bd9541fc132d1d3a979eb903b13046
moonlight <miratorimoonlight@gmail.com>
2019-08-29 16:20:55 +0700

    input_counter

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,38 @@
 // write conde in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+function plusButtonClick()
+{
+    var number = document.getElementById('number')
+    var number_input = Number(document.getElementById('numberInput').value)
+    num = Number(number.textContent)
+    num = num + number_input
+    number.textContent = num
+    var emptyinput = document.getElementById('numberInput')
+    emptyinput.value = ''
+}
+
+function minusButtonClick()
+{
+    var number = document.getElementById('number')
+    var number_input = Number(document.getElementById('numberInput').value)
+    num = Number(number.textContent)
+    num = num - number_input
+    number.textContent = num
+    var emptyinput = document.getElementById('numberInput')
+    emptyinput.value = ''
+}
+
+function resetButtonClick()
+{
+    const number = document.getElementById('number')
+    const number_input = document.getElementById('numberInput')
+    number.textContent = 0
+    number_input.value=''
+}
+
+plusButton.addEventListener('click', plusButtonClick)
+minusButton.addEventListener('click', minusButtonClick)
+resetButton.addEventListener('click', resetButtonClick)
