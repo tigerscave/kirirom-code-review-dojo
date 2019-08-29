9bc0034f27791081836b94019d2b7144b60f8ae2
dalya122899 <darodalya17@kit.edu.kh>
2019-08-29 16:42:22 +0700

    update code

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,29 @@
 // write conde in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const input = document.getElementById('numberInput')
+const numberElement = document.getElementById('number')
+
+
+function changeValue(text){
+	if (text == 'plus')
+	{
+		numberElement.textContent = parseInt(numberElement.textContent) + parseInt(input.value)	
+		input.value = ""
+	}
+	else if (text == 'minus')
+	{
+		numberElement.textContent = parseInt(numberElement.textContent) - parseInt(input.value)
+		input.value = ""
+	}
+	else 
+	{
+		numberElement.textContent = "0"
+	}
+
+}
+
+plusButton.addEventListener('click', () => changeValue('plus'))
+minusButton.addEventListener('click', () => changeValue('minus'))
+resetButton.addEventListener('click', () => changeValue('reset'))
