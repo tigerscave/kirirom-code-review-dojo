28f03f324cfe60b4d613e23ffc55994712ada4b6
VUTHY ZEVO <vuthyzevo@gmail.com>
2019-08-29 16:56:56 +0700

    input-counter commit

--- input-counter/main.js
+++ input-counter/main.js
@@ -1,1 +1,33 @@
 // write conde in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+const numberElement = document.getElementById('number')
+
+let number = parseInt(numberElement.textContent)
+
+plusButton.addEventListener('click', () => {
+    var numInput = document.getElementById('numberInput').value
+    number = number + parseInt(numInput)
+    numberElement.textContent = number
+    document.getElementById('number').numberInput
+    numberInput.value = ''
+})
+
+minusButton.addEventListener('click', () => {
+    var numInput = document.getElementById('numberInput').value
+    number = number - parseInt(numInput)
+    numberElement.textContent = number
+    document.getElementById('number').numberInput
+    numberInput.value = ''
+})
+
+resetButton.addEventListener('click', () => {
+    var numInput = document.getElementById('numberInput').value
+    number = 0
+    numberElement.textContent = number
+    document.getElementById('number').numberInput
+    numberInput.value = ''
+})
+
