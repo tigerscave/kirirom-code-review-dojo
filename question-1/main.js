163683d9de8b49a9df064cec02acd4a174eb6f86
Sokrem Phan <sok.ream06@gmail.com>
2019-08-31 09:33:35 +0700

    implement click button

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,38 @@
 "use strict"
 
 // write conde in here
+
+const appleButton = document.getElementById("apple");
+const textApple = document.getElementById("text");
+const textBanana = document.getElementById("text");
+const bananaButton = document.getElementById("banana");
+const textOrange = document.getElementById("text");
+const orangeButton = document.getElementById("orange");
+const resetButton = document.getElementById("reset");
+const emptyText = document.getElementById("text");
+
+
+function appleButtonClick (){
+	
+	textApple.textContent = "APPLE"
+
+}
+
+function bananaButtonClick (){
+	
+	textBanana.textContent = "BANANA"
+
+}
+function orangeButtonClick (){
+	
+	textOrange.textContent = "ORANGE"
+
+}
+function resetButtonClick() {
+	emptyText.textContent = "-";
+}
+
+resetButton.addEventListener('click', resetButtonClick);
+appleButton.addEventListener('click', appleButtonClick);
+bananaButton.addEventListener('click', bananaButtonClick);
+orangeButton.addEventListener('click', orangeButtonClick);
