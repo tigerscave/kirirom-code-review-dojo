ed187499f63228d410a8408169900a95bc05ceac
sanbunnykit <sanbunny14@kit.edu.kh>
2019-08-31 13:40:17 +0700

    implement simple counter

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,25 @@
 "use strict"
 
-//write code in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const textElement = document.getElementById('number')
+var number = 0;
+function plusButtonClick(){
+	number++;
+	textElement.textContent = number;
+}
+
+function minusButtonClick(){
+	number--;
+	textElement.textContent = number;
+}
+
+function resetButtonClick(){
+	number = 0;
+	textElement.textContent = number;
+}
+
+plusButton.addEventListener('click', plusButtonClick)
+minusButton.addEventListener('click', minusButtonClick)
+resetButton.addEventListener('click', resetButtonClick)
