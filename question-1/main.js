29ada2d27c386c28dd78170bd285037ef42153c6
sanbunnykit <sanbunny14@kit.edu.kh>
2019-08-31 09:47:21 +0700

    change status of property

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,26 @@
 "use strict"
 
-// write conde in here
+const appleButton = document.getElementById('apple')
+const textElement = document.getElementById('text')
+const orangeButton = document.getElementById('orange')
+const bananaButton = document.getElementById('banana')
+const resetButton = document.getElementById('reset')
+
+function appleButtonClick(){
+	textElement.textContent = 'APPLE'
+}
+function bananaButtonClick(){
+	textElement.textContent = 'BANANA'
+}
+function orangeButtonClick(){
+	textElement.textContent = 'ORANGE'
+}
+function resetButtonClick(){
+	textElement.textContent = '-'
+}
+
+
+appleButton.addEventListener('click', appleButtonClick)
+bananaButton.addEventListener('click', bananaButtonClick)
+orangeButton.addEventListener('click', orangeButtonClick)
+resetButton.addEventListener('click', resetButtonClick)
