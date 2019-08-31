f214e6438a3faaa76d9724d840c594b27ce9af28
sanbunnykit <sanbunny14@kit.edu.kh>
2019-08-31 16:39:07 +0700

    Input counter

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,29 @@
 "use strict"
 
-// write conde in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const textElement = document.getElementById('number')
+var getInput = 0;
+var result = 0;
+
+function plusButtonClick(){
+	getInput = parseFloat(document.getElementById('numberInput').value);
+	result = getInput+result;
+	textElement.textContent = result;
+}
+
+function minusButtonClick(){
+	getInput = parseFloat(document.getElementById('numberInput').value);
+	result = result-getInput;
+	textElement.textContent = result;
+}
+
+function resetButtonClick(){
+	result = 0;
+	textElement.textContent = result;
+}
+
+plusButton.addEventListener('click', plusButtonClick)
+minusButton.addEventListener('click', minusButtonClick)
+resetButton.addEventListener('click', resetButtonClick)
