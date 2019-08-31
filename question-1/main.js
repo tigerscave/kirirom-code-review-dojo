60201c2801eb9262b11df8aa3486297c3b888646
Sokrem Phan <sok.ream06@gmail.com>
2019-08-31 09:55:13 +0700

    implement click button

--- question-1/main.js
+++ question-1/main.js
@@ -3,36 +3,18 @@
 // write conde in here
 
 const appleButton = document.getElementById("apple");
-const textApple = document.getElementById("text");
-const textBanana = document.getElementById("text");
 const bananaButton = document.getElementById("banana");
-const textOrange = document.getElementById("text");
 const orangeButton = document.getElementById("orange");
 const resetButton = document.getElementById("reset");
-const emptyText = document.getElementById("text");
+const setText = document.getElementById("text");
 
 
-function appleButtonClick (){
-	
-	textApple.textContent = "APPLE"
-
+function changeText (text) {
+	setText.textContent = text;
 }
 
-function bananaButtonClick (){
-	
-	textBanana.textContent = "BANANA"
+appleButton.addEventListener('click', () => changeText ("APPLE"));
+bananaButton.addEventListener('click', () => changeText ("BANANA"));
+orangeButton.addEventListener('click', () => changeText ("ORANGE"));
+resetButton.addEventListener('click', () => changeText ("-"));
 
-}
-function orangeButtonClick (){
-	
-	textOrange.textContent = "ORANGE"
-
-}
-function resetButtonClick() {
-	emptyText.textContent = "-";
-}
-
-resetButton.addEventListener('click', resetButtonClick);
-appleButton.addEventListener('click', appleButtonClick);
-bananaButton.addEventListener('click', bananaButtonClick);
-orangeButton.addEventListener('click', orangeButtonClick);
