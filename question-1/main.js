925712dc794909c691bb2893e5be371cb8b4587b
subtam <subtam177@gmail.com>
2019-08-31 09:34:45 +0700

    display-text-from-button

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,34 @@
 "use strict"
 
-// write conde in here
+// Constant Variable
+const	appleButton = document.getElementById("apple");
+const	bananaButton = document.getElementById("banana");
+const	orangeButton = document.getElementById("orange");
+const	resetButton = document.getElementById("reset");
+const	textElement = document.getElementById("text")
+
+function appleButtonClick() {
+	// body...
+	textElement.textContent = 'APPLE';
+}
+
+function bananaButtonClick() {
+	// body...
+	textElement.textContent='BANNA';
+}
+
+function orangeButtonClick() {
+	// body...
+	textElement.textContent='ORANGE';
+}
+
+function resetButtonClick() {
+	// body...
+	textElement.textContent='-';
+}
+
+
+appleButton.addEventListener('click',appleButtonClick);
+bananaButton.addEventListener('click',bananaButtonClick);
+orangeButton.addEventListener('click',orangeButtonClick);
+resetButton.addEventListener('click',resetButtonClick);
