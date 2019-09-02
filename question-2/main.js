70387440ff435f9328a60952bff3f1318ffa37a1
DaroLab <labdaro18@kit.edu.kh>
2019-09-03 03:06:01 +0700

    Question-2/LabDaro

--- question-2/main.js
+++ question-2/main.js
@@ -1,23 +1,24 @@
 "use strict"
 
 //write code in here
-const show = document.getElementById('number')
-const Add = document.getElementById('plus')
-const Minus = document.getElementById('minus')
-const Res = document.getElementById('reset')
-let number =0
-Add.addEventListener('click',()=>{
-     number +=1
-    show.textContent = number;}
-)
+const displayText = document.getElementById('number');
+const addBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+const resetBtn = document.getElementById('reset');
+let number = 0;
+
+addBtn.addEventListener('click',()=>{
+     number += 1;
+    displayText.textContent = number;
+});
 
 
-Minus.addEventListener('click',()=>{ 
-    number -=1
-    show.textContent = number;}
-)
+minusBtn.addEventListener('click',()=>{ 
+    number -= 1;
+    displayText.textContent = number;
+});
 
-Res.addEventListener('click',()=>{ 
-    number =0
-    show.textContent = number;}
-)
+resetBtn.addEventListener('click',()=>{ 
+    number = 0;
+    displayText.textContent = number;
+});
