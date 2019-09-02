77ece5f23f63747917061c62d6da90ef7f6456cc
DaroLab <labdaro18@kit.edu.kh>
2019-09-03 02:58:21 +0700

    Question-2/LabDaro

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,16 @@
 "use strict"
 
 // write conde in here
+const appleButton = document.getElementById("apple")
+const bananaButton = document.getElementById("banana")
+const orangeButton = document.getElementById("orange")
+const resetButton = document.getElementById("reset")
+
+function changeText(text){
+    textElement.textContent = text
+}
+appleButton.addEventListener('click',() =>changeText('Apple'))
+bananaButton.addEventListener('click',() =>changeText('Banana'))
+orangeButton.addEventListener('click',() =>changeText('Orange'))
+resetButton.addEventListener('click',() =>changeText('_'))
+
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,23 @@
 "use strict"
 
 //write code in here
+const show = document.getElementById('number')
+const Add = document.getElementById('plus')
+const Minus = document.getElementById('minus')
+const Res = document.getElementById('reset')
+let number =0
+Add.addEventListener('click',()=>{
+     number +=1
+    show.textContent = number;}
+)
+
+
+Minus.addEventListener('click',()=>{ 
+    number -=1
+    show.textContent = number;}
+)
+
+Res.addEventListener('click',()=>{ 
+    number =0
+    show.textContent = number;}
+)
