fb72220d4bee394cf8a81a671397ea47ab3f792b
pagnarithseam18 <seamsopagnarith18@kit.edu.kh>
2019-09-03 11:20:53 +0700

    Update
    Update

--- question-2/main.js
+++ question-2/main.js
@@ -1,6 +1,15 @@
 const plusButton = document.getElementById('plus')
 const minusButton = document.getElementById('minus')
 const resetButton = document.getElementById('reset')
+const addTwoButton = document.getElementById('two')
+const addThreeButton = document.getElementById('three')
+const addFourButton = document.getElementById('four')
+const addFiveButton = document.getElementById('five')
+const addSixButton = document.getElementById('six')
+const addSevenButton = document.getElementById('seven')
+const addEightButton = document.getElementById('eight')
+const addNineButton = document.getElementById('nine')
+const addTenButton = document.getElementById('ten')
 const numberElement = document.getElementById('number')
 
 function plus(){
@@ -15,6 +24,20 @@ function reset(){
 	number.textContent = 0
 }
 
+function add(num)
+{
+	number.textContent = parseInt(number.textContent) + num
+}
+
 plusButton.addEventListener('click', plus)
 minusButton.addEventListener('click', minus)
 resetButton.addEventListener('click', reset)
+addTwoButton.addEventListener('click', () => add(2))
+addThreeButton.addEventListener('click', () => add(3))
+addFourButton.addEventListener('click', () => add(4))
+addFiveButton.addEventListener('click', () => add(5))
+addSixButton.addEventListener('click', () => add(6))
+addSevenButton.addEventListener('click', () => add(7))
+addEightButton.addEventListener('click', () => add(8))
+addNineButton.addEventListener('click', () => add(9))
+addTenButton.addEventListener('click', () => add(10))
