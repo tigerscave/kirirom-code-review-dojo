4ae9f0db23bec3aea636d49a37f1a7a3fd910e84
Serey Vatanak <koeurnsereyvatanak18@kit.edu.kh>
2019-09-03 11:13:22 +0700

    Update main.js

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,30 @@
 "use strict"
 
 // write conde in here
+
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const numberLabel = document.getElementById('number')
+const inputElement = document.getElementById('numberInput')
+let number = 0
+
+function plus(){
+  number = parseInt(numberLabel.textContent)
+  numberLabel.textContent = number + parseInt(inputElement.value)
+}
+
+function minus(){
+  number = parseInt(numberLabel.textContent)
+  numberLabel.textContent = number - parseInt(inputElement.value)
+}
+
+function reset(){
+  numberLabel.textContent = 0
+}
+
+plusButton.addEventListener('click', plus)
+
+minusButton.addEventListener('click', minus)
+
+resetButton.addEventListener('click', reset)
