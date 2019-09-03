d5eb34010653d618a0e74b75a0310a2c9a74f070
Chhunneng <chreachanchhunneng18@kit.edu.kh>
2019-09-03 10:07:44 +0700

    fix code

--- question-2/main.js
+++ question-2/main.js
@@ -2,10 +2,10 @@
 
 //write code in here
 const plusButton = document.getElementById("plus")
-const minusElement = document.getElementById("minus")
+const minusButton = document.getElementById("minus")
 const resetButton = document.getElementById("reset")
 const numberElement = document.getElementById("number")
-var number = 0
+let number = 0
 
 function changeNumber(text){
     if(text==="+")
