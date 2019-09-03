44ee48ed2cd6f862ee2199b49eab4932bf9b6b23
ingvankhinh <47348064+ingvankhinh@users.noreply.github.com>
2019-09-03 10:21:57 +0700

    Hi

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,12 @@
 "use strict"
 
 //write code in here
+
+const textNumber = document.getElementById("number")
+const increment = document.getElementById("plus")
+const decrement = document.getElementById("minus")
+const reset = document.getElementById("reset")
+
+increment.addEventListener('click', () => { textNumber.innerHTML++ })
+decrement.addEventListener('click', () => { textNumber.innerHTML-- })
+reset.addEventListener('click', () => { textNumber.innerHTML = 0 })
