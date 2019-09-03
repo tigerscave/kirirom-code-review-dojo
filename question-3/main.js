e1ae67c51074fb2ebaaca52ff5de29b3985b8c37
livsovannarith <livsovannarith18@kit.edu.kh>
2019-09-03 11:47:47 +0700

    Calculator

--- question-3/main.js
+++ question-3/main.js
@@ -1,17 +1,50 @@
-const plusButton = document.getElementById('plus');
-const minusButton = document.getElementById('minus');
-const resetButton = document.getElementById('reset');
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const inputedNumber = document.getElementById('numberInput')
 const numberElement = document.getElementById('number')
-let number = 0;
-plusButton.addEventListener("click", function(){
-  number++;
-  numberElement.innerHTML = number;
+let firstValue = 0
+let secondValue = 0
+let result = 0
+let direction =0
+plusButton.addEventListener('click',function()
+{ 
+    if (direction == 0) {
+    	firstValue = parseInt(inputedNumber.value)
+    	numberElement.innerHTML = firstValue
+    	inputedNumber.value = " ";
+    	direction++;
+    }
+    else{
+    	secondValue = parseInt(inputedNumber.value)
+    	firstValue = firstValue + secondValue; 
+    	numberElement.innerHTML = firstValue;
+    	secondValue = 0
+    	inputedNumber.value = " "
+    }
 })
-minusButton.addEventListener("click", function(){
-  number--;
-  numberElement.innerHTML = number;
+minusButton.addEventListener('click',function()
+{
+    if (direction == 0) {
+    	firstValue = parseInt(inputedNumber.value)
+    	numberElement.innerHTML = firstValue
+    	inputedNumber.value = " ";
+    	direction++;
+    }
+    else{
+    	secondValue = parseInt(inputedNumber.value)
+    	firstValue = firstValue - secondValue; 
+    	numberElement.innerHTML = firstValue;
+    	secondValue = 0
+    	inputedNumber.value = " "
+    }
 })
-resetButton.addEventListener("click", function(){
-  number = 0;
-  numberElement.innerHTML = number;
+resetButton.addEventListener('click',function()
+{
+    firstValue = 0
+    secondValue = 0
+    direction = 0
+    result = 0
+    inputedNumber.value = " "
+    numberElement.innerHTML = 0
 })
