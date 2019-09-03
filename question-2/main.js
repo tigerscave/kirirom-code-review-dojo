f9d09b9f1504e1b3689656ab518253ca1b86465c
chensokheng <chensokheng18@kit.edu.kh>
2019-09-03 11:18:47 +0700

    :tada: question-3/chensokheng and labdaro

--- question-2/main.js
+++ question-2/main.js
@@ -5,6 +5,8 @@ const numberElement = document.getElementById("number");
 const plusBtn = document.getElementById("plus");
 const minusBtn = document.getElementById("minus");
 const resetBtn = document.getElementById("reset");
+const bodyElement = document.querySelector("body");
+
 let number = 0;
 
 plusBtn.addEventListener("click", () => {
@@ -21,3 +23,27 @@ resetBtn.addEventListener("click", () => {
     number = 0;
     numberElement.textContent = number;
 });
+
+// make the button
+for (let i = 2; i <= 10; i++) {
+    let button = document.createElement("button");
+    button.textContent = i;
+    button.addEventListener("click", () => {
+        number += i;
+        numberElement.textContent = number;
+    });
+    bodyElement.appendChild(button);
+}
+
+// iterate over the button
+
+// select all of the new Btn
+// const allNewBtns = document.querySelectorAll(".newBtn");
+
+// allNewBtns.forEach((btn, index) => {
+//     btn.addEventListener("click", () => {
+//         number += index + 2;
+//         numberElement.textContent = number;
+//     });
+// });
+
--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,32 @@
 "use strict"
 
 // write conde in here
+const numberElement = document.getElementById("number");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const resetBtn = document.getElementById("reset");
+const numberInput = document.getElementById("numberInput");
+let number = 0;
+
+plusBtn.addEventListener("click", () => {
+    number += parseInt(numberInput.value);
+    // numberInput.style.display = "none";
+    numberElement.textContent = number;
+});
+
+minusBtn.addEventListener("click", () => {
+    number -= parseInt(numberInput.value);
+    // numberInput.style.display = "non/e";
+    numberElement.textContent = number;
+});
+
+resetBtn.addEventListener("click", () => {
+    number = 0;
+    numberInput.value = "";
+    numberElement.textContent = number;
+})
+// resetBtn.addEventListener("clic")
+
+
+
+// minusBtn.addEventListener("")
