ebacf02bd8debdf9446b337af166386802e29dcc
Neth Sokreth <nethsokreth18@kit.edu.kh>
2019-09-03 10:32:16 +0700

    :question2done:

--- question-2/main.js
+++ question-2/main.js
@@ -8,16 +8,16 @@ const textView = document.getElementById('number');
 plusBtn.addEventListener('click', plusNum);
 minusBtn.addEventListener('click', minusNum);
 resetBtn.addEventListener('click', reset);
-
+num=0 
 function reset () {
   textView.textContent = 0;
 }
 function plusNum () {
-  let num = parseInt(textView.textContent);
+   num = parseInt(textView.textContent);
   textView.textContent = num + 1;
 }
 function minusNum () {
-  let num = parseInt(textView.textContent);
+   num = parseInt(textView.textContent);
   textView.textContent = num - 1;
 }
 // write code in here
