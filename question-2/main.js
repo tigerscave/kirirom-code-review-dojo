06b80fcbd9d495af5f2252d6b9d296532191b31a
ThyMakra <45727563+ThyMakra@users.noreply.github.com>
2019-09-03 11:37:53 +0700

    question-2 extra and question-3

--- question-2/main.js
+++ question-2/main.js
@@ -1,18 +1,42 @@
 "use strict"
 
-//write code in here
+const body = document.getElementsByTagName("body")[0];
+const numberElement = document.getElementById('number');
 
-const plusButton = document.getElementById('plus');
-const minusButton = document.getElementById('minus');
-const resetButton = document.getElementById('reset');
-const numberElement = document.getElementById('number')
-
-function buttonClicked(operation){
-    if (operation == 'plus') numberElement.textContent = parseInt(numberElement.innerHTML) + 1;
-    else if (operation == 'minus') numberElement.textContent = parseInt(numberElement.innerHTML) - 1;
-    else if (operation == 'reset') numberElement.textContent = 0;
+var i;
+for (i = 2; i <= 10; i++){
+    var button = document.createElement("button");
+    button.innerHTML = i;
+    button.id = i;
+    button.style = "width: 50px; margin: 5px;";
+    body.appendChild(button);        
 }
 
-plusButton.addEventListener('click', () => buttonClicked("plus"));
-minusButton.addEventListener('click', () => buttonClicked("minus"));
-resetButton.addEventListener('click', () => buttonClicked("reset"));
+document.getElementById('plus').addEventListener('click', () => buttonClicked('plus'));
+document.getElementById('minus').addEventListener('click', () => buttonClicked('minus'));
+document.getElementById('reset').addEventListener('click', () => buttonClicked('reset'));
+document.getElementById('2').addEventListener('click', () => buttonClicked('2'));
+document.getElementById('3').addEventListener('click', () => buttonClicked('3'));
+document.getElementById('4').addEventListener('click', () => buttonClicked('4'));
+document.getElementById('5').addEventListener('click', () => buttonClicked('5'));
+document.getElementById('6').addEventListener('click', () => buttonClicked('6'));
+document.getElementById('7').addEventListener('click', () => buttonClicked('7'));
+document.getElementById('8').addEventListener('click', () => buttonClicked('8'));
+document.getElementById('9').addEventListener('click', () => buttonClicked('9'));
+document.getElementById('10').addEventListener('click', () => buttonClicked('10'));
+
+
+function buttonClicked(buttonId){
+    if (buttonId == 'plus'){
+        numberElement.textContent = parseInt(numberElement.innerHTML) + 1;
+    }
+    else if (buttonId == 'minus'){
+        numberElement.textContent = parseInt(numberElement.innerHTML) - 1;        
+    }
+    else if (buttonId == 'reset'){
+        numberElement.textContent = 0;    
+    }
+    else if (buttonId.match(/^-?[0-9]+$/)){
+        numberElement.textContent = parseInt(numberElement.innerHTML) + parseInt(buttonId);
+    }   
+}
--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,16 @@
 "use strict"
 
-// write conde in here
+const numberValue = document.getElementById('number');
+const inputField = document.getElementById('numberInput');
+
+document.getElementById('plus').addEventListener('click', () => buttonClicked('plus'));
+document.getElementById('minus').addEventListener('click', () => buttonClicked('minus'));
+document.getElementById('reset').addEventListener('click', () => buttonClicked('reset'));
+
+function buttonClicked(operate){
+    var input = inputField.value;
+    var number = numberValue.textContent;
+    if (operate == 'plus') numberValue.textContent = parseInt(number) + parseInt(input);
+    else if (operate == 'minus') numberValue.textContent = parseInt(number) - parseInt(input);
+    else if (operate == 'reset') numberValue.textContent = 0;
+}
