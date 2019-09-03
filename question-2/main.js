c69b9f4eb2e0000836dc6a4cf6cff123f2e33f6b
Srean Ponleu <45729895+ponleukit@users.noreply.github.com>
2019-09-03 10:58:05 +0700

    Question-2

--- question-2/main.js
+++ question-2/main.js
@@ -6,6 +6,15 @@ const plusButton = document.getElementById('plus')
 const minusButton = document.getElementById('minus')
 const resetButton = document.getElementById('reset')
 const textResult = document.getElementById('number')
+const twoButton = document.getElementById('plustwo')
+const threeButton = document.getElementById('plusthree')
+const fourButton = document.getElementById('plusfour')
+const fiveButton = document.getElementById('plusfive')
+const sixButton = document.getElementById('plussix')
+const sevenButton = document.getElementById('plusseven')
+const eightButton = document.getElementById('pluseight')
+const nineButton = document.getElementById('plusnine')
+const tenButton = document.getElementById('plusten')
 
 let number = 0
 
@@ -15,4 +24,14 @@ function changeNumber(number){
 
 plusButton.addEventListener('click',()=>changeNumber(number++))
 minusButton.addEventListener('click',()=>changeNumber(number--))
-resetButton.addEventListener('click',()=>changeNumber(0))
+resetButton.addEventListener('click',()=>changeNumber(number=0))
+
+twoButton.addEventListener('click',()=>changeNumber(number += 2))
+threeButton.addEventListener('click',()=>changeNumber(number += 3))
+fourButton.addEventListener('click',()=>changeNumber(number += 4))
+fiveButton.addEventListener('click',()=>changeNumber(number += 5))
+sixButton.addEventListener('click',()=>changeNumber(number += 6))
+sevenButton.addEventListener('click',()=>changeNumber(number += 7))
+eightButton.addEventListener('click',()=>changeNumber(number += 8))
+nineButton.addEventListener('click',()=>changeNumber(number += 9))
+tenButton.addEventListener('click',()=>changeNumber(number += 10))
