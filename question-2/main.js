0d599f8ab047ce56399d7a8c992102fd635080cd
Soknoy <47591457+Gaara-Jinchuriki@users.noreply.github.com>
2019-09-03 10:12:47 +0700

    Update main.js

--- question-2/main.js
+++ question-2/main.js
@@ -5,13 +5,19 @@ const resetButton = document.getElementById("reset")
 
 const numberElement = document.getElementById("number")
 
-plusButton.addEventListener('click', () => {
-    numberElement.innerHTML = parseInt(numberElement.innerHTML) + 1
-})
-minusButton.addEventListener('click', () => {
-    numberElement.innerHTML = parseInt(numberElement.innerHTML) - 1
-})
-resetButton.addEventListener('click', () => {
-    numberElement.innerHTML = 0
-})
+// plusButton.addEventListener('click', () => {
+//     numberElement.innerHTML = parseInt(numberElement.innerHTML) + 1
+// })
+// minusButton.addEventListener('click', () => {
+//     numberElement.innerHTML = parseInt(numberElement.innerHTML) - 1
+// })
+// resetButton.addEventListener('click', () => {
+//     numberElement.innerHTML = 0
+// })
+
+let number = 0
+
+plusButton.addEventListener('click', () => (numberElement.innerHTML = number++))
+minusButton.addEventListener('click', () => (numberElement.innerHTML = number-- ))
+resetButton.addEventListener('click', () => (numberElement.innerHTML = 0))
 //write code in here
