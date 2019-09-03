338c6c570e92657081eb2f7688369089b5d02847
ingvankhinh <47348064+ingvankhinh@users.noreply.github.com>
2019-09-03 11:04:21 +0700

    First try Question 3

--- question-2/main.js
+++ question-2/main.js
@@ -1,11 +1,21 @@
 "use strict"
-
 //write code in here
 const textNumber = document.getElementById("number")
 const plusBtn = document.getElementById("plus")
 const minusBtn = document.getElementById("minus")
 const resetBtn = document.getElementById("reset")
 
-plusBtn.addEventListener('click', () => { textNumber.innerHTML++ })
-minusBtn.addEventListener('click', () => { textNumber.innerHTML-- })
+for (let i = 2; i < 11; i++) {
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = `${i}`;
+    btn.setAttribute("onClick", `changeNum(${i})`)
+    document.body.appendChild(btn);
+}
+
+function changeNum(number) {
+    textNumber.innerHTML = parseInt(textNumber.innerHTML) + number;
+}
+
+plusBtn.addEventListener('click', () => { changeNum(1) })
+minusBtn.addEventListener('click', () => { changeNum(-1) })
 resetBtn.addEventListener('click', () => { textNumber.innerHTML = 0 })
