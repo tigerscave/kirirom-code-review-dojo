ea07ced9497d30dd6fb786c5572c8daf07600e80
Soknoy <47591457+Gaara-Jinchuriki@users.noreply.github.com>
2019-09-03 11:33:55 +0700

    Update main.js

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,26 @@
 "use strict"
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+
+const numberElement = document.getElementById("number")
+const numberInput = document.getElementById("numberInput")
+
+
+plusButton.addEventListener('click', () => {
+    let userInput = parseInt(numberInput.value)
+    let displayNumber = parseInt(numberElement.textContent)
+    numberElement.textContent =  displayNumber + userInput
+    numberInput.value = 0
+})
+minusButton.addEventListener('click', () => {
+    let userInput = parseInt(numberInput.value)
+    let displayNumber = parseInt(numberElement.textContent)
+    numberElement.textContent = displayNumber - userInput
+    numberInput.value = 0
+})
+resetButton.addEventListener('click', () => {
+    numberElement.textContent = 0
+})
 
 // write conde in here
