ead90fcabae433008614409b8630f81ffb6cf176
Taing Theanchhing <theanchhingtaing@gmail.com>
2019-09-03 11:25:34 +0700

    update
    update

--- question-2/main.js
+++ question-2/main.js
@@ -1,18 +1,43 @@
-const plusButton = document.getElementById("plus")
-const minusButton = document.getElementById("minus")
-const resetButton = document.getElementById("reset")
-const numberElement = document.getElementById("number")
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const addTwoButton = document.getElementById('two')
+const addThreeButton = document.getElementById('three')
+const addFourButton = document.getElementById('four')
+const addFiveButton = document.getElementById('five')
+const addSixButton = document.getElementById('six')
+const addSevenButton = document.getElementById('seven')
+const addEightButton = document.getElementById('eight')
+const addNineButton = document.getElementById('nine')
+const addTenButton = document.getElementById('ten')
+const numberElement = document.getElementById('number')
 
-function plus() {
-	number.textContent = parseInt(number.textContent) + 1
+function plus(){
+	numberElement.textContent = parseInt(numberElement.textContent) + 1
 }
-function minus() {
-	number.textContent = parseInt(number.textContent) - 1
+
+function minus(){
+	numberElement.textContent = parseInt(numberElement.textContent) - 1
 }
-function reset() {
-	number.textContent = 0
+
+function reset(){
+	numberElement.textContent = 0
+}
+
+function add(number)
+{
+	numberElement.textContent = parseInt(numberElement.textContent) + number
 }
 
 plusButton.addEventListener('click', plus)
 minusButton.addEventListener('click', minus)
 resetButton.addEventListener('click', reset)
+addTwoButton.addEventListener('click', () => add(2))
+addThreeButton.addEventListener('click', () => add(3))
+addFourButton.addEventListener('click', () => add(4))
+addFiveButton.addEventListener('click', () => add(5))
+addSixButton.addEventListener('click', () => add(6))
+addSevenButton.addEventListener('click', () => add(7))
+addEightButton.addEventListener('click', () => add(8))
+addNineButton.addEventListener('click', () => add(9))
+addTenButton.addEventListener('click', () => add(10))
--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,28 @@
-"use strict"
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const numberElement = document.getElementById('number')
 
-// write conde in here
+function operation(operator)
+{
+	let input = document.getElementById('numberInput')
+	if(operator == "plus")
+	{
+		numberElement.textContent = parseInt(numberElement.textContent) + parseInt(input.value)
+		input.value = ""
+	}
+	else if(operator == "minus")
+	{
+		numberElement.textContent = parseInt(numberElement.textContent) - parseInt(input.value)
+		input.value = ""
+	}
+	else if(operator == "reset")
+	{
+		numberElement.textContent = 0
+		input.value = ""
+	}
+}
+
+plusButton.addEventListener('click', () => operation("plus"))
+minusButton.addEventListener('click', () => operation("minus"))
+resetButton.addEventListener('click', () => operation("reset"))
