f129cce9dd3a29cee73ef0b9f2e12666da23d465
Serey Vatanak <koeurnsereyvatanak18@kit.edu.kh>
2019-09-03 11:55:35 +0700

    Solve Issue

--- question-3/main.js
+++ question-3/main.js
@@ -7,19 +7,31 @@ const minusButton = document.getElementById('minus')
 const resetButton = document.getElementById('reset')
 const numberLabel = document.getElementById('number')
 const inputElement = document.getElementById('numberInput')
+let numberResult = 0
 
 function plus(){
-  numberLabel.textContent = parseInt(numberLabel.textContent) + parseInt(inputElement.value)
+  numberResult = parseInt(numberLabel.textContent) + parseInt(inputElement.value)
+  nanCheck(numberResult)
   inputElement.value = ''
 }
 
 function minus(){
-  numberLabel.textContent = parseInt(numberLabel.textContent) - parseInt(inputElement.value)
+  numberResult = parseInt(numberLabel.textContent) - parseInt(inputElement.value)
+  nanCheck(numberResult)
   inputElement.value = ''
 }
 
 function reset(){
   numberLabel.textContent = 0
+}
+
+function nanCheck(numberResult){
+  if(isNaN(numberResult)){
+    alert('Invalid Input')
+  }
+  else{
+    numberLabel.textContent = numberResult
+  }
 }
 
 plusButton.addEventListener('click', plus)
