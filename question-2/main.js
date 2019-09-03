166627ba8a2ed2897d07325db1e6321d24d7217f
Putheareak <45678262+Putheareak@users.noreply.github.com>
2019-09-03 12:08:31 +0700

    question-2/Peng Putheareak
    Task Complete

--- question-2/main.js
+++ question-2/main.js
@@ -2,13 +2,50 @@ const plusButton = document.getElementById("plus")
 const minusButton = document.getElementById("minus")
 const resetButton = document.getElementById("reset")
 const numberElement = document.getElementById("number")
-
 let number = 0
+for(let i=2; i<=10; i++){
+    let  button = document.createElement("BUTTON")
+    button.innerHTML = i
+    button.setAttribute("onclick",`plus${i}()`)
+    document.body.appendChild(button)
+}
+function plus2(){
+    number += 2
+    numberElement.textContent = number
+}
+function plus3(){
+    number += 3
+    numberElement.textContent = number
+}
+function plus4(){
+    number += 4
+    numberElement.textContent = number
+}
+function plus5(){
+    number += 5
+    numberElement.textContent = number
+}
+function plus6(){
+    number += 6
+    numberElement.textContent = number
+}
+function plus7(){
+    number += 7
+    numberElement.textContent = number
+}
+function plus8(){
+    number += 8
+    numberElement.textContent = number
+}
+function plus9(){
+    number += 9
+    numberElement.textContent = number
+}
+function plus10(){
+    number += 10
+    numberElement.textContent = number
+}
 
-plusButton.addEventListener("click",() => {numberelement.textContent = ++number})
-minusButton.addEventListener("click",() => {numberelement.textContent = --number})
-resetButton.addEventListener("click",() => {numberelement.textContent = 0})
-
-
-
-//write code in here
+plusButton.addEventListener("click",() => { numberElement.textContent = ++number})
+minusButton.addEventListener("click",() => { numberElement.textContent = --number})
+resetButton.addEventListener("click",() => { numberElement.textContent = 0})
--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,30 @@
-"use strict"
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+const numberElement = document.getElementById("number")
 
-// write conde in here
+let numbers = 0
+
+function calculate(string){
+    let numberInputElement = document.getElementById("numberInput").value
+    if(isNaN(numberInputElement)){
+        alert("Invalid Error")
+    }
+    if (string == "+"){
+        numbers = numbers + Number(numberInputElement)
+        numberElement.textContent = numbers
+    }
+    else{   
+        numbers = numbers - Number(numberInputElement)
+        numberElement.textContent = numbers
+    }
+}
+function reset(){
+    numbers = 0
+    numberElement.textContent = numbers
+
+}
+
+plusButton.addEventListener("click",() => calculate("+"))
+minusButton.addEventListener("click",() => calculate("-"))
+resetButton.addEventListener("click",() => reset())
