54979af849840ee5fb79184b45027d837a2eb136
Hun Vikran <hunvikran18@kit.edu.kh>
2019-09-03 10:32:08 +0700

    question2Done

--- question-2/main.js
+++ question-2/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// write code in here
 const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
 const resetBtn = document.getElementById('reset');
@@ -9,15 +10,17 @@ plusBtn.addEventListener('click', plusNum);
 minusBtn.addEventListener('click', minusNum);
 resetBtn.addEventListener('click', reset);
 
+let num = 0;
+
 function reset () {
   textView.textContent = 0;
 }
 function plusNum () {
-  let num = parseInt(textView.textContent);
+  num = parseInt(textView.textContent);
   textView.textContent = num + 1;
 }
 function minusNum () {
-  let num = parseInt(textView.textContent);
+  num = parseInt(textView.textContent);
   textView.textContent = num - 1;
 }
 // write code in here
