48ba4a46ba4aa1012d93fce5fef94553b42d6604
Saosopheabattambang <48276451+Saosopheabattambang@users.noreply.github.com>
2019-09-03 10:12:12 +0700

    Add files via upload

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,22 @@
 "use strict"
+const plusbutton = document.getElementById("plus")
+const minusbutton = document.getElementById("minus")
+const resetbutton = document.getElementById("reset")
+const numberbutton = document.getElementById("number")
 
-//write code in here
+function buttonClick(res)
+{
+    if(res=='plus'){
+        numberbutton.textContent = parseInt(numberbutton.innerHTML)+1
+    }
+    else if (res == 'minus'){
+        numberbutton.textContent = parseInt(numberbutton.innerHTML)-1
+    }
+    else if (res == 'reset'){
+        numberbutton.textContent = 0
+
+    }
+}
+plusbutton.addEventListener('click', ()=> buttonClick('plus'))
+minusbutton.addEventListener('click', ()=> buttonClick('minus'))
+resetbutton.addEventListener('click', ()=> buttonClick('reset'))
