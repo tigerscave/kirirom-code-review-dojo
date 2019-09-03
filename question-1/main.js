edd2b53d444c5bc3381f00b2397fca7d5e6e1521
Vireak Sok <sokvireak2000@gmail.com>
2019-09-03 10:29:49 +0700

    question-2/vireak

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,11 @@
-"use strict"
+"use strict";
 
-// write conde in here
+// write cone in here
+const appleButton = document.getElementById("apple");
+const textElement = document.getElementById("text").innerHTML;
+console.log(textElement);
+function changeText(text) {
+  textElement.textContent = text;
+}
+//evaluation strategy
+// evaluate all argument from left to Right
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,15 @@
-"use strict"
+"use strict";
 
 //write code in here
+const increaseButton = document.getElementById("plus");
+const decreaseButton = document.getElementById("minus");
+const resetButton = document.getElementById("reset");
+const text = document.getElementById("number");
+var number = parseInt(document.getElementById("number").innerText);
+
+function calculate(number) {
+  text.textContent = number;
+}
+increaseButton.addEventListener("click", () => calculate((number += 1)));
+decreaseButton.addEventListener("click", () => calculate((number -= 1)));
+resetButton.addEventListener("click", () => calculate(0));
