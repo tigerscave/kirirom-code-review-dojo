c7af3d35b722d5b8aeb4344307e73ebb152cfe57
CrimsonPiZza <45735628+CrimsonPiZza@users.noreply.github.com>
2019-09-03 09:29:04 +0700

    Answer

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,9 @@
 "use strict"
-
 // write conde in here
+
+const text = document.getElementById("text")
+
+function change(id){
+    const theButton = document.getElementById(id)
+    text.innerHTML = theButton.value
+} 
