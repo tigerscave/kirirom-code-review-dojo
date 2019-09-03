c5e186d4f6eeda59e1f1650788c0737eb396a7f9
chensokheng <chensokheng18@kit.edu.kh>
2019-09-03 09:53:23 +0700

    question-2/chensokheng :tada:

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,26 @@
 "use strict"
 
 // write conde in here
+const appleButton = document.getElementById("apple");
+const bananaButton = document.getElementById("button");
+const orangeBtn = document.getElementById("orange");
+const resetBtn = document.getElementById("reset");
+const textElement = document.getElementById("text");
+
+
+
+appleButton.addEventListener("click", () => {
+    textElement.textContent = "APPLE";
+});
+
+bananaButton.addEventListener('click', () => {
+    textElement.textContent = "BANANA";
+});
+
+orangeBtn.addEventListener('click', () => {
+    textElement.textContent = "ORGANE";
+});
+
+resetBtn.addEventListener('click', () => {
+    textElement.textContent = "-";
+});
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,23 @@
 "use strict"
 
 //write code in here
+const diplayNumber = document.getElementById("number");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const resetBtn = document.getElementById("reset");
+let number = 0;
+
+plusBtn.addEventListener("click", () => {
+    number += 1;
+    diplayNumber.textContent = number;
+});
+
+minusBtn.addEventListener("click", () => {
+    number -= 1;
+    diplayNumber.textContent = number;
+});
+
+resetBtn.addEventListener("click", () => {
+    number = 0;
+    diplayNumber.textContent = number;
+})
