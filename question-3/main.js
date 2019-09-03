05664d021cafe14d8e4400dd62e2939a954eb56a
CrimsonPiZza <45735628+CrimsonPiZza@users.noreply.github.com>
2019-09-03 11:29:39 +0700

    Input Counter, question 3, first attempt succeed!

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,36 @@
 "use strict"
 
 // write conde in here
+
+let getElement = id => document.getElementById(id)
+
+const inputBox = getElement("numberInput")
+const numberText = getElement("number")
+
+const plusBtn = getElement("plus")
+const minusBtn = getElement("minus")
+const resetBtn = getElement("reset")
+
+function changeNumber(operator,step){
+    //Set up varibles
+    if (step == "") step = 0 ; else step = parseInt(step)
+    let currentNum = parseInt(numberText.innerHTML)
+
+    switch(operator){
+        case "+" :
+            numberText.innerHTML = currentNum + step
+            break;
+
+        case "-" :
+            numberText.innerHTML = currentNum - step
+            break;
+        case "reset" :
+            numberText.innerHTML = step
+            break;
+    }
+    inputBox.value = ""
+}
+
+plusBtn.addEventListener("click", ()=>changeNumber("+",inputBox.value))
+minusBtn.addEventListener("click", ()=>changeNumber("-",inputBox.value))
+resetBtn.addEventListener("click", ()=>changeNumber("reset",0))
