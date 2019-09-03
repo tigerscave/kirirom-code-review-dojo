c32786e46ca7569c2cca3279924ef13bf8f27fae
Hun Vikran <hunvikran18@kit.edu.kh>
2019-09-03 11:44:26 +0700

    betterVariableName

--- question-3/main.js
+++ question-3/main.js
@@ -5,7 +5,7 @@ const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
 const resetBtn = document.getElementById('reset');
 const textView = document.getElementById('number');
-const editText = document.getElementById('numberInput');
+const textInput = document.getElementById('numberInput');
 
 plusBtn.addEventListener('click', plusNum);
 minusBtn.addEventListener('click', minusNum);
@@ -13,18 +13,18 @@ resetBtn.addEventListener('click', reset);
 
 function reset () {
   textView.textContent = 0;
-  editText.value = '';
+  textInput.value = '';
 }
 function plusNum () {
-  if (editText.value) {
-    textView.textContent = parseInt(textView.textContent) + parseInt(editText.value);
-    editText.value = '';
+  if (textInput.value) {
+    textView.textContent = parseInt(textView.textContent) + parseInt(textInput.value);
+    textInput.value = '';
   }
 }
 function minusNum () {
-  if (editText.value) {
-    textView.textContent = parseInt(textView.textContent) - parseInt(editText.value);
-    editText.value = '';
+  if (textInput.value) {
+    textView.textContent = parseInt(textView.textContent) - parseInt(textInput.value);
+    textInput.value = '';
   }
 }
 // write code in here
