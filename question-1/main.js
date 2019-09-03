ed39ba430a634d6a7bfcc2ecc0d0952a61043177
Chor Visalrotanak <chorvisalrotanak18@kit.edu.kh>
2019-09-03 09:59:47 +0700

    question-2/Visalrotanak

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,20 @@
 "use strict"
 
 // write conde in here
+
+const appleButton = document.getElementById('apple')
+const bananaButton = document.getElementById('banana')
+const orangeButton = document.getElementById('orange')
+const resetButton = document.getElementById('reset')
+
+const textElement = document.getElementById('text')
+
+function ChangeText(text){
+    textElement.textContent = text
+}
+
+
+appleButton.addEventListener('click',() => ChangeText("APPLE"))
+bananaButton.addEventListener('click',() => ChangeText("BANANA"))
+orangeButton.addEventListener('click',() => ChangeText("ORANGE"))
+resetButton.addEventListener('click',() => ChangeText("-"))
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,28 @@
 "use strict"
 
 //write code in here
+
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const textElement = document.getElementById('number')
+
+let num = 0
+function func(id){
+    switch(id){
+        case "plus":
+            num+=1
+            break
+        case "minus":
+            num-=1
+            break
+        case "reset":
+            num = 0
+    }
+    textElement.innerHTML = num
+
+}
+
+plusButton.addEventListener('click', () => func("plus"))
+minusButton.addEventListener('click', () => func("minus"))
+resetButton.addEventListener('click',() => func("reset"))
