e843a3079a07ff334e900a155e2a074156ba91b1
SOKHUONG <uonsokhuong18@kit.edu.kh>
2019-09-03 11:27:57 +0700

    add  1 2

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,19 @@
 "use strict"
 
+
+
 // write conde in here
+const appleButton=document.getElementById("apple")
+const bananaButton=document.getElementById("banana")
+const orangeButton=document.getElementById("orange")
+const textElement=document.getElementById("text")
+const resetButton=document.getElementById("reset")
+function ButtonClick(text){
+    textElement.textContent=text
+}
+
+appleButton.addEventListener("click",()=>  ButtonClick("APPLE"))
+bananaButton.addEventListener("click",()=>   ButtonClick("BANANA"))
+orangeButton.addEventListener("click",()=>   ButtonClick("ORANGE"))
+resetButton.addEventListener("click",()=>  ButtonClick("-"))
+
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,18 @@
 "use strict"
 
 //write code in here
+
+const plusButton=document.getElementById("plus")
+const minusButton=document.getElementById("minus")
+const text=document.getElementById("number")
+const resetButton=document.getElementById("reset")
+let number=0
+function change(todo){
+    if(todo=="add"){number++}
+    if(todo=="sub"){number--}
+    if(todo=="reset"){number=0}
+    text.textContent=number
+}
+plusButton.addEventListener("click", ()=>    change("add"))
+minusButton.addEventListener("click", ()=>   change("sub"))
+resetButton.addEventListener("click", ()=>   change("reset"))
