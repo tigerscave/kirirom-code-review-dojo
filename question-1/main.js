c7c259cb1868871e7e847a0ba460f609861f6ed5
Soknoy <47591457+Gaara-Jinchuriki@users.noreply.github.com>
2019-09-03 09:59:30 +0700

    question-2/Soknoy

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,29 @@
 "use strict"
+// document.getElementById("apple").addEventListener("click", () => {
+//     document.getElementById("text").innerHTML = document.getElementById("apple").innerHTML
+// })
+// document.getElementById("orange").addEventListener("click", () => {
+//     document.getElementById("text").innerHTML = document.getElementById("orange").innerHTML
+// })
+// document.getElementById("banana").addEventListener("click", () => {
+//     document.getElementById("text").innerHTML = document.getElementById("banana").innerHTML
+// })
+// document.getElementById("reset").addEventListener("click", () => {
+//     document.getElementById("text").innerHTML = "-"
+// })
 
+const appleButton = document.getElementById("apple")
+const orangeButton = document.getElementById("orange")
+const bananaButton = document.getElementById("banana")
+const resetButton = document.getElementById("reset")
+
+const textElement = document.getElementById("text")
+
+function changetext(text){
+    textElement.innerHTML = text
+}
+appleButton.addEventListener('click', () => changetext('APPLE'))
+orangeButton.addEventListener('click', () => changetext('BANANA'))
+bananaButton.addEventListener('click', () => changetext('ORANGE'))
+resetButton.addEventListener('click', () => changetext('-'))
 // write conde in here
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,17 @@
 "use strict"
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
 
+const numberElement = document.getElementById("number")
+
+plusButton.addEventListener('click', () => {
+    numberElement.innerHTML = parseInt(numberElement.innerHTML) + 1
+})
+minusButton.addEventListener('click', () => {
+    numberElement.innerHTML = parseInt(numberElement.innerHTML) - 1
+})
+resetButton.addEventListener('click', () => {
+    numberElement.innerHTML = 0
+})
 //write code in here
