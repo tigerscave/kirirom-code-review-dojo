40f5f28a45f1a70be8e37a0e7b6d8bafdceb3b34
Sovichea Nhim <nhimsovichea18@kit.edu.kh>
2019-09-03 11:29:29 +0700

    Solved the exercise

--- question-2/main.js
+++ question-2/main.js
@@ -2,19 +2,25 @@
 
 //write code in here
 const number = document.getElementById("number")
-const plusBtn = document.getElementById("plus")
-const minusBtn = document.getElementById("minus")
-const resetBtn = document.getElementById("reset")
+const plusBtn = document.getElementById("plusBtn")
+const minusBtn = document.getElementById("minusBtn")
+const resetBtn = document.getElementById("resetBtn")
+const numberTwo = document.getElementById('twoBtn')
+const numberThree = document.getElementById('twoBtn')
+const numberTwo = document.getElementById('twoBtn')
+const numberTwo = document.getElementById('twoBtn')
+const numberTwo = document.getElementById('twoBtn')
+const numberTwo = document.getElementById('twoBtn')
+const numberTwo = document.getElementById('twoBtn')
+const numberTwo = document.getElementById('twoBtn')
 let value = 0
 
-plusBtn.addEventListener("click", () =>{
-    number.textContent = value++
-})
+var array = Array("plus", "minus", "reset", 2, 3, 4, 5, 6, 7,8 )
+for (let i = 0; i < array.length; i++) {
+    var button = document.createElement('button')
+    button.textContent = array[i]
+    button.id = array[i]+"Btn"
+    let body = document.getElementsByTagName('body')[0]
+    body.append(button)
+}
 
-minusBtn.addEventListener("click", () => {
-    number.textContent = value--
-})
-
-resetBtn.addEventListener("click", () => {
-    number.textContent = 0
-}) 
--- question-3/main.js
+++ question-3/main.js
@@ -2,19 +2,25 @@
 
 //write code in here
 const number = document.getElementById("number")
+const numberInput = document.getElementById("numberInput")
 const plusBtn = document.getElementById("plus")
 const minusBtn = document.getElementById("minus")
 const resetBtn = document.getElementById("reset")
-var value = 0
 
-plusBtn.addEventListener("click", function(){
-    number.innerHTML = value++
+plusBtn.addEventListener("click", () => { 
+    let userInput = parseInt(document.getElementById("numberInput").value)
+    let displayNumber = parseInt(document.getElementById("number").textContent)
+    number.textContent = displayNumber + userInput
+    numberInput.value = 0
 })
 
-minusBtn.addEventListener("click", function(){
-    number.innerHTML = value--
-})
+minusBtn.addEventListener("click", () => { 
+    let userInput = parseInt(document.getElementById("numberInput").value)
+    let displayNumber = parseInt(document.getElementById("number").textContent)
+    number.textContent = displayNumber - userInput
+    numberInput.value = 0
+ })
 
-resetBtn.addEventListener("click", function(){
-    number.innerHTML = 0
-}) 
+resetBtn.addEventListener("click", () => { 
+    number.textContent = 0
+    numberInput.textContent = 0 }) 
