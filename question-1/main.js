3ba2f0d2eeba0a0e8dc9b5ce20f72fe3e74846a7
MENGHEANG_OU <mengheang_ou@MENGHEANGOUsMBP.pfsense.int>
2019-09-03 10:20:44 +0700

    Name

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,19 @@
 "use strict"
 
 // write conde in here
+
+const appleButton = document.getElementById('apple')
+const bananaButton = document.getElementById('banana')
+const oragneButton = document.getElementById('orange')
+const resetButton = document.getElementById('reset')
+const textElement = document.getElementById('text')
+
+function changeText(text)
+{
+    textElement.textContent = text;
+}
+
+appleButton.addEventListener('click', () => changeText("APPLE"))
+bananaButton.addEventListener('click',() => changeText("BANANA"))
+oragneButton.addEventListener('click',() => changeText("ORANGE"))
+resetButton.addEventListener('click', () => changeText("_"))
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,20 @@
 "use strict"
 
 //write code in here
+
+var i = 0
+document.getElementById('plus').addEventListener('click',function()
+{
+    i++;
+    document.getElementById('number').innerHTML = i
+})
+document.getElementById('minus').addEventListener('click',function()
+{
+    i--;
+    document.getElementById('number').innerHTML = i
+})
+document.getElementById('reset').addEventListener('click',function()
+{
+    i = 0;
+    document.getElementById('number').innerHTML =  i
+})
