6923d5cc55111572fde7ea7c457d94b1093fc054
Hun Vikran <hunvikran18@kit.edu.kh>
2019-09-03 10:14:31 +0700

    :tada: questionDone

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,14 @@
-"use strict"
-
+'use strict';
+const appleButton = document.getElementById('apple');
+const bananaButton = document.getElementById('banana');
+const orangeButton = document.getElementById('orange');
+const resetButton = document.getElementById('reset');
+const textView = document.getElementById('text');
+appleButton.addEventListener('click', () => changeText('Apple'));
+bananaButton.addEventListener('click', () => changeText('Banana'));
+orangeButton.addEventListener('click', () => changeText('Orange'));
+resetButton.addEventListener('click', () => changeText(''));
+function changeText (text) {
+  textView.textContent = text;
+}
 // write conde in here
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,23 @@
-"use strict"
+'use strict';
 
-//write code in here
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+const resetBtn = document.getElementById('reset');
+const textView = document.getElementById('number');
+
+plusBtn.addEventListener('click', () => plusNum());
+minusBtn.addEventListener('click', () => minusNum());
+resetBtn.addEventListener('click', reset);
+
+function reset () {
+  textView.textContent = 0;
+}
+function plusNum () {
+  let num = parseInt(textView.textContent);
+  textView.textContent = num + 1;
+}
+function minusNum () {
+  let num = parseInt(textView.textContent);
+  textView.textContent = num - 1;
+}
+// write code in here
