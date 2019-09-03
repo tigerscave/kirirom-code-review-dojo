6e21f4e6f4e8f1ff9170f167879aed4283d0370a
Visal Yun <yunvisal18@kit.edu.kh>
2019-09-03 11:38:00 +0700

    question-3/VisalYun

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,27 @@
-"use strict"
+const addButton = document.getElementById('plus');
+const minusButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+const text = document.getElementById('number');
 
-// write conde in here
+let number = 0;
+let inputNumber = 0;
+
+function calculateAndDisplay(sign){
+	number = parseInt(document.getElementById('number').innerText);
+	inputNumber = parseInt(document.getElementById('numberInput').value);
+	if(sign=="+"){
+		number+=inputNumber;
+	}
+	else if(sign=="-"){
+		number-=inputNumber;
+	}
+	else{
+		number=0;
+	}
+	document.getElementById('numberInput').value="";
+	text.textContent = number;
+}
+
+addButton.addEventListener('click',()=>calculateAndDisplay("+"));
+minusButton.addEventListener('click',()=>calculateAndDisplay("-"));
+resetButton.addEventListener('click',()=>calculateAndDisplay(0));
