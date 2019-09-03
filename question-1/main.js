e90f190f5c2f409e0fb74a42667ba695fae942e7
Serey Vatanak <koeurnsereyvatanak18@kit.edu.kh>
2019-09-03 09:22:39 +0700

    Update main.js

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,15 @@
 "use strict"
 
-// write conde in here
+// write code in here
+
+const appleButton = document.getElementById('apple')
+
+const bananaButton = document.getElementById('banana')
+
+const textElement = document.getElementById('text')
+
+function appleButtonClick(){
+  alert('hello apple')
+}
+
+appleButton addEventListener('click', appleButtonClick)
