43015d1ebcd91c302cee42c2bd0913319468a505
pagnarithseam18 <seamsopagnarith18@kit.edu.kh>
2019-09-03 10:23:51 +0700

    Update main.js

--- question-1/main.js
+++ question-1/main.js
@@ -1,29 +1,8 @@
-"use strict"
-
-// write conde in here
-
 const appleButton = document.getElementById('apple')
 const bananaButton = document.getElementById('banana')
 const orangeButton = document.getElementById('orange')
 const resetButton = document.getElementById('reset')
 const textElement = document.getElementById('text')
-
-
-function appleButtonClick(){
-	textElement.textContent = "APPLE"
-}
-
-function bananaButtonClick(){
-	textElement.textContent = "BANANA"
-}
-
-function orangeButtonClick(){
-	textElement.textContent = "ORANGE"
-}
-
-function resetButtonClick(){
-	textElement.textContent = "-"
-}
 
 function changeText(text)
 {
