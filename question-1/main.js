8ef1f2ba1621f40a7545e4442a72ed36351069f8
Srean Ponleu <sreanponleu18@kit.edu.kh>
2019-09-03 09:28:36 +0700

    question-1

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,17 @@
 "use strict"
 
 // write conde in here
+const appleButton = document.getElementById('apple')
+const bananaButton = document.getElementById('banana')
+const orangeButton = document.getElementById('orange')
+const resetButton = document.getElementById('reset')
+
+const textElement = document.getElementById('text')
+
+function changeText(text){
+    textElement.textContent = text;
+}
+appleButton.addEventListener('click', () => changeText('APPLE'))
+bananaButton.addEventListener('click', () => changeText('BANANA'))
+orangeButton.addEventListener('click', () => changeText('ORANGE'))
+resetButton.addEventListener('click', () => changeText('-'))
