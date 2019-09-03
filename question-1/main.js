7eb9593c925b537417d3fbd31cf21258623edfb2
Putheareak <45678262+Putheareak@users.noreply.github.com>
2019-09-03 09:58:59 +0700

    question-2/Peng Putheareak

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,11 @@
-"use strict"
+const appleButton = document.getElementById("apple")
+const bananaButton = document.getElementById("banana")
+const orangeButton = document.getElementById("orange")
+const resetButton = document.getElementById("reset")
+
+
+function changetext(text){
+    text.textContent = 
+}
 
 // write conde in here
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,14 @@
-"use strict"
+const plusbtn = document.getElementById("plus")
+const minusbtn = document.getElementById("minus")
+const resetbtn = document.getElementById("reset")
+const numberbtn = document.getElementById("number")
+
+
+var number = 0
+plusbtn.addEventListener("click", ()=> {numberbtn.textContent=++number})
+minusbtn.addEventListener("click", ()=> {numberbtn.textContent=--number})
+resetbtn.addEventListener("click", ()=> {numberbtn.textContent=0})
+
+
 
 //write code in here
