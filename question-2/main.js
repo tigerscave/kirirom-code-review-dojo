3d29bf604898094f952588de5dd7b168ccc9a78b
livsovannarith <livsovannarith18@kit.edu.kh>
2019-09-03 10:18:32 +0700

    second commit

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,13 @@
-"use strict"
-
-//write code in here
+var a = 0;
+document.getElementById('plus').addEventListener("click", function(){
+  a++;
+  document.getElementById('number').innerHTML = a;
+})
+document.getElementById('minus').addEventListener("click", function(){
+  a--;
+  document.getElementById('number').innerHTML = a;
+})
+document.getElementById('reset').addEventListener("click", function(){
+  a=0;
+  document.getElementById('number').innerHTML = a;
+})
