308d2431b327834392c49823ae52ad70077223c7
livsovannarith <livsovannarith18@kit.edu.kh>
2019-09-03 11:04:15 +0700

    Third function changed

--- question-2/main.js
+++ question-2/main.js
@@ -1,16 +1,17 @@
 const plusButton = document.getElementById('plus');
 const minusButton = document.getElementById('minus');
 const resetButton = document.getElementById('reset');
+const numberElement = document.getElementById('number')
 let number = 0;
 plusButton.addEventListener("click", function(){
   number++;
-  document.getElementById('number').innerHTML = number;
+  numberElement.innerHTML = number;
 })
 minusButton.addEventListener("click", function(){
   number--;
-  document.getElementById('number').innerHTML = number;
+  numberElement.innerHTML = number;
 })
 resetButton.addEventListener("click", function(){
   number = 0;
-  document.getElementById('number').innerHTML = number;
+  numberElement.innerHTML = number;
 })
--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,17 @@
-"use strict"
-
-// write conde in here
+const plusButton = document.getElementById('plus');
+const minusButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+const numberElement = document.getElementById('number')
+let number = 0;
+plusButton.addEventListener("click", function(){
+  number++;
+  numberElement.innerHTML = number;
+})
+minusButton.addEventListener("click", function(){
+  number--;
+  numberElement.innerHTML = number;
+})
+resetButton.addEventListener("click", function(){
+  number = 0;
+  numberElement.innerHTML = number;
+})
