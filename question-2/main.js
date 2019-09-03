6fb2e9755c44b016c9ec5a635180d995a7a291e5
Saosopheabattambang <48276451+Saosopheabattambang@users.noreply.github.com>
2019-09-03 12:15:25 +0700

    Add files via upload

--- question-2/main.js
+++ question-2/main.js
@@ -14,9 +14,37 @@ function buttonClick(operation)
     }
     else if (operation == 'reset'){
         numberbutton.textContent = 0
-
+    }
+    else{
+        i = 0
+        for(i=2; i<=10; i++)
+        {
+            if(operation==''+i+'')
+            {
+                numberbutton.textContent = parseInt(numberbutton.innerHTML) + i
+            }
+        }
     }
 }
 plusbutton.addEventListener('click', ()=> buttonClick('plus'))
 minusbutton.addEventListener('click', ()=> buttonClick('minus'))
 resetbutton.addEventListener('click', ()=> buttonClick('reset'))
+
+
+var i = 0
+for(i=2; i<=10 ;i++){
+    var button = document.createElement("button")
+    button.innerHTML = i
+    button.id = i
+    button.style= "width: 50px; margin: 5px;";
+    document.body.appendChild(button)
+}
+document.getElementById('2').addEventListener('click', ()=> buttonClick('2'))
+document.getElementById('3').addEventListener('click', ()=> buttonClick('3'))
+document.getElementById('4').addEventListener('click', ()=> buttonClick('4'))
+document.getElementById('5').addEventListener('click', ()=> buttonClick('5'))
+document.getElementById('6').addEventListener('click', ()=> buttonClick('6'))
+document.getElementById('7').addEventListener('click', ()=> buttonClick('7'))
+document.getElementById('8').addEventListener('click', ()=> buttonClick('8'))
+document.getElementById('9').addEventListener('click', ()=> buttonClick('9'))
+document.getElementById('10').addEventListener('click', ()=> buttonClick('10'))
--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,27 @@
-"use strict"
 
-// write conde in here
+const numberValue = document.getElementById('number');
+const inputField = document.getElementById('numberInput');
+
+document.getElementById('plus').addEventListener('click', () => buttonClicked('plus'));
+document.getElementById('minus').addEventListener('click', () => buttonClicked('minus'));
+document.getElementById('reset').addEventListener('click', () => buttonClicked('reset'));
+
+function buttonClicked(operate){
+    var input = parseInt(inputField.value);
+    var number = parseInt(numberValue.textContent);
+    
+        if (operate == 'reset') 
+        {
+            numberValue.textContent = 0;     
+        } 
+        else if(isNaN(input))
+        {
+            alert("Invalid error")
+        }
+        else{
+            if(operate=='plus') numberValue.textContent = number + input;
+            else if(operate =='minus') numberValue.textContent = number - input;
+        }
+        inputField.value = '';
+    }    
+    
