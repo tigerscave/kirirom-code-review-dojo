335550087160116153ee4579e408efea3ded938a
ingvankhinh <47348064+ingvankhinh@users.noreply.github.com>
2019-09-03 11:24:25 +0700

    3rd challenge

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,16 @@
 "use strict"
 
 // write conde in here
+const textNumber = document.getElementById("number")
+const input = document.getElementById("numberInput")
+const plusBtn = document.getElementById("plus")
+const minusBtn = document.getElementById("minus")
+const resetBtn = document.getElementById("reset")
+
+function changeNum(number) {
+    textNumber.innerHTML = parseInt(textNumber.innerHTML) + parseInt(number)
+}
+
+plusBtn.addEventListener('click', () => { changeNum(input.value) })
+minusBtn.addEventListener('click', () => { changeNum(`-${input.innerHTML}`) })
+reset.addEventListener('click', () => { textNumber.innerHTML = 0 })
