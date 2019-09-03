f2dfdb8875b5af3863cb05ab2c6f289fc9b69e08
Neth Sokreth <nethsokreth18@kit.edu.kh>
2019-09-03 11:50:52 +0700

    betterFunctionName

--- question-3/main.js
+++ question-3/main.js
@@ -4,28 +4,26 @@ const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
 const resetBtn = document.getElementById('reset');
 const textView = document.getElementById('number');
-const editTextValue = document.getElementById('numberInput');
+const textInput = document.getElementById('numberInput');
 
-plusBtn.addEventListener('click', () => plusNum());
-minusBtn.addEventListener('click', () => minusNum());
+plusBtn.addEventListener('click', addNum);
+minusBtn.addEventListener('click', subtractNum);
 resetBtn.addEventListener('click', reset);
 
 function reset() {
   textView.textContent = 0;
-  editTextValue.value = '';
+  textInput.value = '';
 }
-function plusNum() {
-  if (editTextValue.value) {
-    const currNum = parseInt(textView.textContent);
-    textView.textContent = currNum + parseInt(editTextValue.value);
-    editTextValue.value = '';
+function addNum() {
+  if (textInput.value) {
+    textView.textContent = parseInt(textView.textContent) + parseInt(textInput.value);
+    textInput.value = '';
   }
 }
-function minusNum() {
-  if (editTextValue.value) {
-    const currNum = parseInt(textView.textContent);
-    textView.textContent = currNum - parseInt(editTextValue.value);
-    editTextValue.value = '';
+function subtractNum() {
+  if (textInput.value) {
+    textView.textContent = parseInt(textView.textContent) - parseInt(textInput.value);
+    textInput.value = '';
   }
 }
 // write conde in here
