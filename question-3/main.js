3bade7d70afd34e372b78a72e604aad380c735a1
MENGHEANG_OU <mengheang_ou@MENGHEANGOUsMBP.pfsense.int>
2019-09-03 11:47:47 +0700

    question-3

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,56 @@
 "use strict"
 
 // write conde in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const inputedNumber = document.getElementById('numberInput')
+const numberElement = document.getElementById('number')
+let firstValue = 0
+let secondValue = 0
+let result = 0
+let direction = 0
+plusButton.addEventListener('click',function()
+{ 
+    if(direction == 0)
+    {
+        firstValue = parseInt(inputedNumber.value)
+        numberElement.innerHTML = firstValue
+        inputedNumber.value = " ";
+        direction++;
+    }
+    else
+    {
+        secondValue = parseInt(inputedNumber.value)
+        firstValue = firstValue + secondValue
+        numberElement.innerHTML = firstValue
+        secondValue = 0
+        inputedNumber.value = " ";
+    }   
+})
+minusButton.addEventListener('click',function()
+{
+    if(direction == 0)
+    {
+        firstValue = parseInt(inputedNumber.value)
+        numberElement.innerHTML = firstValue
+        inputedNumber.value = " ";
+        direction++;
+    }
+    else
+    {
+        secondValue = parseInt(inputedNumber.value)
+        firstValue = firstValue - secondValue
+        numberElement.innerHTML = firstValue
+        secondValue = 0
+        inputedNumber.value = " ";
+    } 
+})
+resetButton.addEventListener('click',function()
+{
+    firstValue = 0
+    secondValue = 0
+    direction = 0
+    result = 0
+    numberElement.innerHTML = 0
+})
