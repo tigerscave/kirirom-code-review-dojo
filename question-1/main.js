cc8f2ff3c2c296386ae6e6efa01aba70ccb66817
Chhunneng <chreachanchhunneng18@kit.edu.kh>
2019-09-03 10:01:32 +0700

    add code

--- question-1/main.js
+++ question-1/main.js
@@ -8,20 +8,11 @@ const orangeButton = document.getElementById("orange")
 const resetButton = document.getElementById("reset")
 const textElement = document.getElementById("text")
 
-function appleButtonClick(){
-    textElement.textContent = "APPLE"
-}
-function bananaButtonClick(){
-    textElement.textContent = "BANANA"
-}
-function orangeButtonClick(){
-    textElement.textContent = "ORANGE"
-}
-function resetButtonClick(){
-    textElement.textContent = "-"
+function changeText(text){
+	textElement.textContent=text
 }
 
-appleButton.addEventListener("click",appleButtonClick)
-bananaButton.addEventListener("click",bananaButtonClick)
-orangeButton.addEventListener("click",orangeButtonClick)
-resetButton.addEventListener("click",resetButtonClick)
+appleButton.addEventListener("click",() => changeText("APPLE"))
+bananaButton.addEventListener("click",() => changeText("BANANA"))
+orangeButton.addEventListener("click",() => changeText("ORANGE"))
+resetButton.addEventListener("click",() => changeText("-"))
--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,28 @@
 "use strict"
 
 //write code in here
+const plusButton = document.getElementById("plus")
+const minusElement = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+const numberElement = document.getElementById("number")
+var number = 0
+
+function changeNumber(text){
+    if(text==="+")
+    {
+        numberElement.textContent=++number
+    }
+    else if(text==="-")
+    {
+        numberElement.textContent=--number
+    }
+    else
+    {
+        numberElement.textContent=0
+    }
+}
+
+plusButton.addEventListener("click",() => changeNumber("+"))
+minusElement.addEventListener("click",() => changeNumber("-"))
+resetButton.addEventListener("click",() => changeNumber("reset"))
+
