5c2d32e88870b684deba701261f1a8105007ff51
PhanSovanna <phansovanna18@kit.edu.kh>
2019-09-03 10:04:35 +0700

    question 2

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,29 @@
 "use strict"
 
 //write code in here
+
+const minus = document.getElementById("minus")
+const add = document.getElementById("plus")
+const reset = document.getElementById("reset")
+const number = document.getElementById("number")
+var n = 0
+
+function clicked(id){
+    switch(id)
+    {
+        case "minus":
+            n--
+            break
+        case "add":
+            n++
+            break
+        case "reset":
+            n = 0
+            break
+    }
+    number.innerHTML = n
+}
+
+add.addEventListener("click", () => clicked("add"))
+minus.addEventListener("click", () => clicked("minus"))
+reset.addEventListener("click", () => clicked("reset"))
