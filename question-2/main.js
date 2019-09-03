d77405139dd07eb4162398c266bb8a940f814eb0
Visal Yun <yunvisal18@kit.edu.kh>
2019-09-03 10:30:11 +0700

    question-2/VisalYun

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,14 @@
-"use strict"
+const increaseButton = document.getElementById('plus');
+const decreaseButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+const text = document.getElementById('number');
 
-//write code in here
+var number = parseInt(document.getElementById('number').innerText);
+
+function calculate(n){
+	text.textContent = number;
+
+}
+increaseButton.addEventListener('click',()=>calculate(number+=1));
+decreaseButton.addEventListener('click',()=>calculate(number-=1));
+resetButton.addEventListener('click',()=>calculate(0))
