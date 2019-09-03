c2fd47699becd4189c73b039494cb47d466e5dab
CrimsonPiZza <45735628+CrimsonPiZza@users.noreply.github.com>
2019-09-03 09:57:55 +0700

    Hey done!

--- question-2/main.js
+++ question-2/main.js
@@ -1,3 +1,16 @@
 "use strict"
 
 //write code in here
+
+
+function change(id){
+    const number = document.getElementById("number")
+    switch(id){
+        case "plus" : number.innerHTML = parseInt(number.innerHTML) + 1
+        break;
+        case "minus" : number.innerHTML = parseInt(number.innerHTML) - 1
+        break;
+        case "reset" : number.innerHTML = 0
+        break;
+    }
+}
