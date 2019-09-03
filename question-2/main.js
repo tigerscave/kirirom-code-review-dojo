db4bcf2d98483234318d25acacea0e47f27c19f3
Serey Vatanak <koeurnsereyvatanak18@kit.edu.kh>
2019-09-03 10:34:46 +0700

    Update main.js

--- question-2/main.js
+++ question-2/main.js
@@ -5,20 +5,20 @@
 const plusButton = document.getElementById('plus')
 const minusButton = document.getElementById('minus')
 const resetButton = document.getElementById('reset')
-const number = document.getElementById('number')
+const numberLabel = document.getElementById('number')
 
 function plus(){
-  let num = parseInt(number.textContent)
-  number.textContent = num+1
+  let number = parseInt(numberLabel.textContent)
+  numberLabel.textContent = number+1
 }
 
 function minus(){
-  let num = parseInt(number.textContent)
-  number.textContent = num-1
+  let number = parseInt(numberLabel.textContent)
+  numberLabel.textContent = number-1
 }
 
 function reset(){
-  number.textContent = 0
+  numberLabel.textContent = 0
 }
 
 plusButton.addEventListener('click', plus)
