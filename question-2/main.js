faccf0633575cd052b776ac201326540a8f83813
CrimsonPiZza <45735628+CrimsonPiZza@users.noreply.github.com>
2019-09-03 10:07:42 +0700

    Hi

--- question-2/main.js
+++ question-2/main.js
@@ -1,16 +1,13 @@
 "use strict"
 
 //write code in here
+const plusButt = document.getElementById("plus")
+const minusButt = document.getElementById("minus")
+const resetButt = document.getElementById("reset")
 
+const number = document.getElementById("number")
 
-function change(id){
-    const number = document.getElementById("number")
-    switch(id){
-        case "plus" : number.innerHTML = parseInt(number.innerHTML) + 1
-        break;
-        case "minus" : number.innerHTML = parseInt(number.innerHTML) - 1
-        break;
-        case "reset" : number.innerHTML = 0
-        break;
-    }
-}
+plusButt.addEventListener("click",() => number.innerHTML = parseInt(number.innerHTML) + 1)
+minusButt.addEventListener("click",() => number.innerHTML = parseInt(number.innerHTML) - 1)
+resetButt.addEventListener("click",() => number.innerHTML = 0)
+
