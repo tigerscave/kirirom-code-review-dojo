f43730f512509cb80d2af9d3f62e85de6d0ca6fa
Sokhon Chhun <47809100+SokhonC@users.noreply.github.com>
2019-09-03 11:47:58 +0700

    question-3

--- question-3/main.js
+++ question-3/main.js
@@ -1,3 +1,29 @@
-"use strict"
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const textResult = document.getElementById('number')
 
-// write conde in here
+let number = 0 
+let getNumber = 0
+
+function changeSign(sign){
+    getNumber=parseInt(document.getElementById('numberInput').value)
+    number=parseInt(document.getElementById('number').innerText)
+    switch(sign){
+        case '+':
+            number+=getNumber
+            break
+        case '-':
+            number-=getNumber
+            break
+        case 'reset':
+            number=0
+    }
+    document.getElementById('numberInput').value=""
+    textResult.textContent=number
+}
+plusButton.addEventListener('click',()=>changeSign('+'))
+minusButton.addEventListener('click',()=>changeSign('-'))
+resetButton.addEventListener('click',()=>changeSign('reset'))
+
+
