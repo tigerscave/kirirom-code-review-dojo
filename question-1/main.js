66f21b2e9a2a49fbb87ddcc692235cd64744d47f
pagnarithseam18 <seamsopagnarith18@kit.edu.kh>
2019-09-03 09:22:17 +0700

    Update main.js

--- question-1/main.js
+++ question-1/main.js
@@ -1,3 +1,30 @@
 "use strict"
 
 // write conde in here
+
+const appleButton = document.getElementById('apple')
+const bananaButton = document.getElementById('banana')
+const orangeButton = document.getElementById('orange')
+const resetButton = document.getElementById('reset')
+const textElement = document.getElementById('text')
+
+
+function appleButtonClick(){
+	textElement.textContent = "APPLE"
+}
+
+function bananaButtonClick(){
+	textElement.textContent = "BANANA"
+}
+
+function orangeButtonClick(){
+	textElement.textContent = "ORANGE"
+}
+
+function resetButtonClick(){
+	textElement.textContent = "-"
+}
+appleButton.addEventListener('click', appleButtonClick)
+bananaButton.addEventListener('click', bananaButtonClick)
+orangeButton.addEventListener('click', orangeButtonClick)
+resetButton.addEventListener('click', resetButtonClick)
