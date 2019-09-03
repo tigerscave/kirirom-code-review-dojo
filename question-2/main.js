175b5e6c067c2f3558e70df7d9ef6caa7bdcc18b
Chor Visalrotanak <chorvisalrotanak18@kit.edu.kh>
2019-09-03 10:27:27 +0700

    question 2

--- question-2/main.js
+++ question-2/main.js
@@ -7,17 +7,17 @@ const minusButton = document.getElementById('minus')
 const resetButton = document.getElementById('reset')
 const textElement = document.getElementById('number')
 
-let num = 0
+let number = 0
 function ChangeValue(operation){
     switch(operation){
         case plusButton:
-            num+=1
+            number+=1
             break
         case minusButton:
-            num-=1
+            number-=1
             break
         case resetButton:
-            num = 0
+            number = 0
     }
     textElement.innerHTML = num
 
